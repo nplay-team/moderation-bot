2b059b482794679f1f1fe69af9d8c5b8650a2c4c
SachsenspieltCoding <coding@sachsenspielt.de>
2025-01-19 17:01:36 +0100

    awaiting #24 to be merged

--- src/main/java/de/nplay/moderationbot/moderation/modlog/ModlogCommands.java
+++ src/main/java/de/nplay/moderationbot/moderation/modlog/ModlogCommands.java
@@ -1,10 +1,12 @@
 package de.nplay.moderationbot.moderation.modlog;
 
+import com.github.kaktushose.jda.commands.annotations.Inject;
 import com.github.kaktushose.jda.commands.annotations.interactions.Interaction;
 import com.github.kaktushose.jda.commands.annotations.interactions.Param;
 import com.github.kaktushose.jda.commands.annotations.interactions.Permissions;
 import com.github.kaktushose.jda.commands.annotations.interactions.SlashCommand;
 import com.github.kaktushose.jda.commands.dispatching.events.interactions.CommandEvent;
+import com.github.kaktushose.jda.commands.embeds.EmbedCache;
 import de.nplay.moderationbot.moderation.ModerationService;
 import de.nplay.moderationbot.permissions.BotPermissionFlags;
 import net.dv8tion.jda.api.Permission;
@@ -13,6 +15,9 @@ import net.dv8tion.jda.api.entities.Member;
 @Interaction
 @Permissions(BotPermissionFlags.MODLOG_READ)
 public class ModlogCommands {
+
+    @Inject
+    EmbedCache embedCache;
     
     @SlashCommand(value = "moderation modlog", desc = "Zeigt den Modlog eines Mitglieds an", isGuildOnly = true, enabledFor = Permission.BAN_MEMBERS)
     public void modlog(CommandEvent event, @Param("Der Member, dessen Modlog abgerufen werden soll") Member member) {
@@ -20,6 +25,8 @@ public class ModlogCommands {
                 .stream()
                 .filter(act -> act.userId() == Long.parseLong(member.getId()))
                 .toList();
+
+        event.reply(ModlogService.getModlogEmbedHeader(embedCache, member));
     }
 
 }
--- src/main/java/de/nplay/moderationbot/moderation/modlog/ModlogService.java
+++ src/main/java/de/nplay/moderationbot/moderation/modlog/ModlogService.java
@@ -1,5 +1,29 @@
 package de.nplay.moderationbot.moderation.modlog;
 
+import com.github.kaktushose.jda.commands.embeds.EmbedCache;
+import com.github.kaktushose.jda.commands.embeds.EmbedDTO;
+import de.nplay.moderationbot.embeds.EmbedColors;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+
+import java.time.temporal.ChronoField;
+import java.util.Set;
+
 public class ModlogService {
 
+    public static EmbedDTO getModlogEmbedHeader(EmbedCache embedCache, Member member) {
+        return embedCache.getEmbed("modlogHeader")
+                .injectValue("username", member.getUser().getName())
+                .injectValue("userId", member.getIdLong())
+                .injectValue("avatarUrl", member.getUser().getEffectiveAvatarUrl())
+                .injectValue("roles", member.getRoles().stream().map(Role::getName).reduce((a, b) -> a + ", " + b).orElse("Keine Rollen"))
+                .injectValue("createdAt", member.getTimeCreated().getLong(ChronoField.INSTANT_SECONDS))
+                .injectValue("joinedAt", member.getTimeJoined().getLong(ChronoField.INSTANT_SECONDS))
+                .injectValue("color", EmbedColors.DEFAULT);
+    }
+
+    public static Set<String> getModlogMessages() {
+        return Set.of("message1", "message2", "message3");
+    }
+    
 }
