f642099f382d026bc0bcd2c934909dcee2581c50
SachsenspieltCoding <coding@sachsenspielt.de>
2025-03-02 17:45:10 +0100

    changes

--- src/main/java/de/nplay/moderationbot/config/ConfigCommands.java
+++ src/main/java/de/nplay/moderationbot/config/ConfigCommands.java
@@ -1,8 +1,8 @@
 package de.nplay.moderationbot.config;
 
 import com.github.kaktushose.jda.commands.annotations.Inject;
-import com.github.kaktushose.jda.commands.annotations.interactions.Choices;
 import com.github.kaktushose.jda.commands.annotations.interactions.Interaction;
+import com.github.kaktushose.jda.commands.annotations.interactions.Param;
 import com.github.kaktushose.jda.commands.annotations.interactions.Permissions;
 import com.github.kaktushose.jda.commands.annotations.interactions.SlashCommand;
 import com.github.kaktushose.jda.commands.dispatching.events.interactions.CommandEvent;
@@ -11,6 +11,8 @@ import de.nplay.moderationbot.config.bot.BotConfig;
 import de.nplay.moderationbot.embeds.EmbedColors;
 import de.nplay.moderationbot.permissions.BotPermissions;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
 @Interaction
 @Permissions(BotPermissions.ADMINISTRATOR)
@@ -19,27 +21,19 @@ public class ConfigCommands {
     @Inject
     private EmbedCache embedCache;
 
-    @SlashCommand(value = "config set", desc = "Setzt eine Konfigurations-Variable", isGuildOnly = true, enabledFor = Permission.ADMINISTRATOR)
-    public void setConfig(CommandEvent event, @Choices({"spielersucheAusschlussRolle", "serverlogKanal"}) String name, String value) {
-        ConfigService.set(name, value);
-        event.reply(embedCache.getEmbed("configSet").injectValue("key", name).injectValue("value", value).injectValue("color", EmbedColors.SUCCESS));
+    @SlashCommand(value = "config set spielersuche-ausschluss-rolle", desc = "Setzt eine Konfigurations-Variable", isGuildOnly = true, enabledFor = Permission.ADMINISTRATOR)
+    public void setSpielersucheAusschlussRolle(CommandEvent event, @Param("Die Rolle, welche der User erhalten soll, wenn er von der Spielersuche ausgeschlossen wird.") Role role) {
+        onConfigSet(event, BotConfig.SPIELERSUCHE_AUSSCHLUSS_ROLLE, role.getId());
     }
 
-    @SlashCommand(value = "config get", desc = "Gibt den Wert einer Konfigurations-Variable zurück", isGuildOnly = true, enabledFor = Permission.ADMINISTRATOR)
-    public void getConfig(CommandEvent event, @Choices({"spielersucheAusschlussRolle", "serverlogKanal"}) String name) {
-        var botConfig = ConfigService.get(name);
+    @SlashCommand(value = "config set serverlog-kanal", desc = "Setzt eine Konfigurations-Variable", isGuildOnly = true, enabledFor = Permission.ADMINISTRATOR)
+    public void setConfig(CommandEvent event, @Param("Der Text-Kanal in denen die Bot-Logs gesendet werden sollen.") TextChannel channel) {
+        onConfigSet(event, BotConfig.SERVERLOG_KANAL, channel.getId());
+    }
 
-        if (botConfig.isEmpty()) {
-            event.reply(embedCache.getEmbed("configNotSet").injectValue("key", name).injectValue("color", EmbedColors.ERROR));
-            return;
-        }
-
-        event.reply(
-                embedCache.getEmbed("configGet")
-                        .injectValue("key", name)
-                        .injectValue("value", name)
-                        .injectValue("color", EmbedColors.DEFAULT)
-        );
+    void onConfigSet(CommandEvent event, BotConfig config, String value) {
+        ConfigService.set(config, value);
+        event.reply(embedCache.getEmbed("configSet").injectValue("key", config).injectValue("value", value).injectValue("color", EmbedColors.SUCCESS));
     }
 
     @SlashCommand(value = "config list", desc = "Listet alle Konfigurations-Variablen auf", isGuildOnly = true, enabledFor = Permission.ADMINISTRATOR)
@@ -50,7 +44,7 @@ public class ConfigCommands {
 
         configs.forEach(config -> {
             var value = ConfigService.get(config);
-            embed.addField(config, value.orElse("Nicht gesetzt"), false);
+            embed.addField(config.toString(), value.orElse("Nicht gesetzt"), false);
         });
 
         event.jdaEvent().replyEmbeds(embed.build()).queue();
--- src/main/java/de/nplay/moderationbot/config/ConfigService.java
+++ src/main/java/de/nplay/moderationbot/config/ConfigService.java
@@ -2,22 +2,23 @@ package de.nplay.moderationbot.config;
 
 import de.chojo.sadu.queries.api.call.Call;
 import de.chojo.sadu.queries.api.query.Query;
+import de.nplay.moderationbot.config.bot.BotConfig;
 
 import java.util.Optional;
 
 public class ConfigService {
 
-    public static Optional<String> get(String name) {
+    public static Optional<String> get(BotConfig config) {
         return Query.query("SELECT value FROM configs WHERE name = ?")
-                .single(Call.of().bind(name))
+                .single(Call.of().bind(config))
                 .mapAs(java.lang.String.class)
                 .first();
     }
 
-    public static void set(String name, String value) {
+    public static void set(BotConfig config, String value) {
         Query.query("INSERT INTO configs (name, value) VALUES (?, ?) ON CONFLICT (name) DO UPDATE SET value = EXCLUDED.value")
                 .single(Call.of()
-                        .bind(name)
+                        .bind(config)
                         .bind(value)
                 )
                 .insert();
