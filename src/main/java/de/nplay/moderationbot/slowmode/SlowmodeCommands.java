a2b25d25cb71e1af413b12ca0e60b37fc4fbe7f1
SachsenspieltCoding <coding@sachsenspielt.de>
2025-04-24 12:34:26 +0200

    support forum channels

--- src/main/java/de/nplay/moderationbot/slowmode/SlowmodeCommands.java
+++ src/main/java/de/nplay/moderationbot/slowmode/SlowmodeCommands.java
@@ -2,12 +2,14 @@ package de.nplay.moderationbot.slowmode;
 
 import com.github.kaktushose.jda.commands.annotations.interactions.Command;
 import com.github.kaktushose.jda.commands.annotations.interactions.Interaction;
+import com.github.kaktushose.jda.commands.annotations.interactions.Optional;
 import com.github.kaktushose.jda.commands.annotations.interactions.Param;
 import com.github.kaktushose.jda.commands.dispatching.events.interactions.CommandEvent;
 import com.github.kaktushose.jda.commands.embeds.EmbedCache;
 import com.google.inject.Inject;
 import de.nplay.moderationbot.Helpers;
 import de.nplay.moderationbot.embeds.EmbedColors;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 
 import java.time.Duration;
 
@@ -19,37 +21,55 @@ public class SlowmodeCommands {
 
     @Command(value = "slowmode info", desc = "Gibt Informationen zu den aktuellen Slowmode-Einstellungen zurück oder setzt diese.")
     public void slowmodeInfoCommand(
-            CommandEvent event
+            CommandEvent event,
+            @Optional @Param("Der Channel, für den die Informationen angezeigt werden sollen.") GuildChannel channel
     ) {
-        var slowmode = SlowmodeService.getSlowmode(event.getGuildChannel());
+        var guildChannel = channel == null ? event.getGuildChannel() : channel;
+        var slowmode = SlowmodeService.getSlowmode(guildChannel);
         if (slowmode.isPresent()) {
             event.reply(
                     embedCache.getEmbed("slowmodeInfo")
+                            .injectValue("channelId", guildChannel.getId())
                             .injectValue("color", EmbedColors.DEFAULT)
                             .injectValue("duration", Helpers.durationToString(Duration.ofSeconds(slowmode.get().duration()), true))
             );
         } else {
-            event.reply(embedCache.getEmbed("slowmodeNotSet").injectValue("color", EmbedColors.DEFAULT));
+            event.reply(
+                    embedCache.getEmbed("slowmodeNotSet")
+                            .injectValue("channelId", guildChannel.getId())
+                            .injectValue("color", EmbedColors.DEFAULT)
+            );
         }
     }
 
     @Command(value = "slowmode set", desc = "Gibt Informationen zu den aktuellen Slowmode-Einstellungen zurück oder setzt diese.")
     public void slowmodeSetCommand(
             CommandEvent event,
-            @Param("Wie lang soll der Slowmode sein?") Duration duration
+            @Param("Wie lang soll der Slowmode sein?") Duration duration,
+            @Optional @Param("Der Channel, für den der Slowmode gesetzt werden soll.") GuildChannel channel
     ) {
-        SlowmodeService.setSlowmode(event.getGuildChannel(), (int) duration.toSeconds());
+        var guildChannel = channel == null ? event.getGuildChannel() : channel;
+        SlowmodeService.setSlowmode(guildChannel, (int) duration.toSeconds());
         event.reply(
                 embedCache.getEmbed("slowmodeSet")
+                        .injectValue("channelId", guildChannel.getId())
                         .injectValue("color", EmbedColors.SUCCESS)
                         .injectValue("duration", Helpers.durationToString(duration))
         );
     }
 
     @Command(value = "slowmode remove", desc = "Entfernt den Slowmode für diesen Channel.")
-    public void slowmodeRemoveCommand(CommandEvent event) {
-        SlowmodeService.removeSlowmode(event.getGuildChannel());
-        event.reply(embedCache.getEmbed("slowmodeRemove").injectValue("color", EmbedColors.SUCCESS));
+    public void slowmodeRemoveCommand(
+            CommandEvent event,
+            @Optional @Param("Der Channel, für den der Slowmode entfernt werden soll.") GuildChannel channel
+    ) {
+        var guildChannel = channel == null ? event.getGuildChannel() : channel;
+        SlowmodeService.removeSlowmode(guildChannel);
+        event.reply(
+                embedCache.getEmbed("slowmodeRemove")
+                        .injectValue("channelId", guildChannel.getId())
+                        .injectValue("color", EmbedColors.SUCCESS)
+        );
     }
 
 }
--- src/main/java/de/nplay/moderationbot/slowmode/SlowmodeEventHandler.java
+++ src/main/java/de/nplay/moderationbot/slowmode/SlowmodeEventHandler.java
@@ -4,20 +4,24 @@ import com.github.kaktushose.jda.commands.embeds.EmbedCache;
 import de.nplay.moderationbot.Helpers;
 import de.nplay.moderationbot.embeds.EmbedColors;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Optional;
 
 public class SlowmodeEventHandler extends ListenerAdapter {
-
     private static final Logger log = LoggerFactory.getLogger(SlowmodeEventHandler.class);
-
-    private EmbedCache embedCache;
+    private final EmbedCache embedCache;
 
     public SlowmodeEventHandler(EmbedCache embedCache) {
         this.embedCache = embedCache;
@@ -25,38 +29,88 @@ public class SlowmodeEventHandler extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.isFromGuild()) return;
-        if (event.getAuthor().isBot()) return;
-        if (event.getMember().hasPermission(Permission.MANAGE_CHANNEL)) return;
+        if (!isValidUser(event.getMember())) return;
 
         var channel = event.getGuildChannel();
-
+        var message = event.getMessage();
+        var author = event.getAuthor();
         var slowmode = SlowmodeService.getSlowmode(channel);
 
-        if (slowmode.isPresent()) {
-            Optional<Message> lastMessage = event.getGuild().getTextChannelById(channel.getId())
-                    .getIterableHistory()
-                    .stream()
-                    .filter(message -> message.getAuthor().getId().equals(event.getAuthor().getId()))
-                    .filter(message -> !message.getId().equals(event.getMessageId()))
-                    .findFirst();
+        if (slowmode.isEmpty()) return;
 
-            if (lastMessage.isPresent()) {
-                if (event.getMessage().getTimeCreated().toInstant().toEpochMilli() - lastMessage.get().getTimeCreated().toInstant().toEpochMilli() < slowmode.get().duration() * 1000L) {
-                    event.getMessage().delete().queue();
-                    log.info("Deleted message from {} in {} because of slowmode", event.getAuthor().getAsTag(), channel.getName());
-                    event.getAuthor().openPrivateChannel().queue(privateChannel -> {
-                        privateChannel.sendMessageEmbeds(
-                                embedCache.getEmbed("slowmodeMessageRemoved")
-                                        .injectValue("channelId", channel.getId())
-                                        .injectValue("duration", Helpers.durationToString(Duration.ofSeconds(slowmode.get().duration()), true))
-                                        .injectValue("timestampLastMessage", lastMessage.get().getTimeCreated().toInstant().getEpochSecond())
-                                        .injectValue("color", EmbedColors.ERROR)
-                                        .toMessageEmbed()
-                        ).queue();
-                    });
-                }
-            }
-        }
+        Optional<Message> lastMessage = event.getGuild()
+                .getTextChannelById(channel.getId())
+                .getIterableHistory()
+                .stream()
+                .filter(it -> author.getId().equals(it.getAuthor().getId()))
+                .filter(it -> !it.getId().equals(message.getId()))
+                .filter(it -> isWithinSlowmode(message.getTimeCreated().toInstant(), it.getTimeCreated().toInstant(), slowmode.get().duration()))
+                .findFirst();
+
+        if (lastMessage.isEmpty()) return;
+
+        message.delete().queue();
+        notifyUser(
+                author,
+                channel.getId(),
+                slowmode.get().duration(),
+                lastMessage.get().getTimeCreated().toInstant().getEpochSecond()
+        );
+    }
+
+    @Override
+    public void onChannelCreate(ChannelCreateEvent event) {
+        if (event.getChannel().getType() != ChannelType.GUILD_PUBLIC_THREAD) return;
+
+        var thread = event.getChannel().asThreadChannel();
+        if (thread.getParentChannel().getType() != ChannelType.FORUM) return;
+        if (!isValidUser(thread.getOwner())) return;
+
+        var forumChannel = thread.getParentChannel().asForumChannel();
+        var slowmode = SlowmodeService.getSlowmode(forumChannel);
+
+        if (slowmode.isEmpty()) return;
+
+        Optional<ThreadChannel> lastPost = event.getGuild()
+                .getThreadChannels()
+                .stream()
+                .filter(it -> forumChannel.getId().equals(it.getParentChannel().getId()))
+                .filter(it -> thread.getOwnerId().equals(it.getOwnerId()))
+                .filter(it -> !thread.getId().equals(it.getId()))
+                .filter(it -> isWithinSlowmode(thread.getTimeCreated().toInstant(), it.getTimeCreated().toInstant(), slowmode.get().duration()))
+                .findFirst();
+
+
+        if (lastPost.isEmpty()) return;
+
+        thread.delete().queue();
+        notifyUser(
+                thread.getOwner().getUser(),
+                forumChannel.getId(),
+                slowmode.get().duration(),
+                lastPost.get().getTimeCreated().toInstant().getEpochSecond()
+        );
+    }
+
+    private boolean isValidUser(Member member) {
+        return member != null && !member.getUser().isBot() && !member.hasPermission(Permission.MANAGE_CHANNEL);
+    }
+
+    private boolean isWithinSlowmode(Instant current, Instant last, long slowmodeSeconds) {
+        return current.toEpochMilli() - last.toEpochMilli() < slowmodeSeconds * 1000L;
+    }
+
+    private void notifyUser(User user, String channelId, long duration, long lastMessageTimestamp) {
+        user.openPrivateChannel().queue(privateChannel -> {
+            privateChannel.sendMessageEmbeds(
+                    embedCache.getEmbed("slowmodeMessageRemoved")
+                            .injectValue("channelId", channelId)
+                            .injectValue("duration", Helpers.durationToString(Duration.ofSeconds(duration), true))
+                            .injectValue("timestampLastMessage", lastMessageTimestamp)
+                            .injectValue("color", EmbedColors.ERROR)
+                            .toMessageEmbed()
+            ).queue();
+        });
     }
 }
+
