138feb92b70928a6b4801279940ed42263c900c0
SachsenspieltCoding <coding@sachsenspielt.de>
2025-06-05 16:20:21 +0200

    added months to duration to string

--- src/main/java/de/nplay/moderationbot/Helpers.java
+++ src/main/java/de/nplay/moderationbot/Helpers.java
@@ -13,15 +13,17 @@ public class Helpers {
         StringBuilder builder = new StringBuilder();
 
         long years = duration.toDays() >= 365 ? Math.floorDiv(duration.toDays(), 365) : 0;
-        long days = duration.toDays() - (years * 365);
+        long months = duration.toDays() >= 30 ? Math.floorDiv(duration.toDays(), 30) : 0;
+        long days = duration.toDays() - (years * 365) - (months * 30);
         long hours = duration.toHours() % 24;
         long minutes = duration.toMinutes() % 60;
         long seconds = duration.getSeconds() % 60;
 
         if (years > 0) builder.append(years).append("y ");
+        if (months > 0) builder.append(months).append("m ");
         if (days > 0) builder.append(days).append("d ");
         if (hours > 0) builder.append(hours).append("h ");
-        if (minutes > 0) builder.append(minutes).append("m ");
+        if (minutes > 0) builder.append(minutes).append("min ");
         if (seconds > 0) builder.append(seconds).append("s ");
 
         return builder.toString().trim();
@@ -31,9 +33,10 @@ public class Helpers {
         if (detailed) {
             return durationToString(duration)
                     .replaceAll("y", " Jahr(e)")
+                    .replaceAll("m ", " Monat(e) ")
                     .replaceAll("d", " Tag(e)")
                     .replaceAll("h", " Stunde(n)")
-                    .replaceAll("m", " Minute(n)")
+                    .replaceAll("min", " Minute(n)")
                     .replaceAll("s", " Sekunde(n)");
         } else {
             return durationToString(duration);
