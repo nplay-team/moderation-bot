1f80c7be38657e9b403ca52c195a5c7c82c47e00
Kaktushose <matti.psk@gmail.com>
2025-08-28 14:16:39 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     application
-    id("com.gradleup.shadow") version "8.3.5"
+    id("com.gradleup.shadow") version "9.0.2"
 }
 
 application.mainClass = "de.nplay.moderationbot.Bootstrapper"
@@ -20,12 +20,12 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
-    implementation("net.dv8tion:JDA:5.3.0") {
+    implementation("net.dv8tion:JDA:5.6.1") {
         exclude(module = "opus-java")
     }
 
     implementation("club.minnced:discord-webhooks:0.8.4")
-    implementation("io.github.kaktushose:jda-commands:4.0.0-beta.7")
+    implementation("io.github.kaktushose:jda-commands:4.0.0-beta.8")
     implementation("ch.qos.logback:logback-core:1.5.13")
     implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("org.slf4j:slf4j-api:2.0.9")
@@ -47,7 +47,7 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.isIncremental = true
     options.compilerArgs.add("-parameters")
-    sourceCompatibility = "23"
+    sourceCompatibility = "24"
 }
 
 tasks.withType<ShadowJar> {
