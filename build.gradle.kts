d8c8e389d824ef4f5bfea79a4ffd425dabb15636
Kaktushose <matti.psk@gmail.com>
2025-08-30 15:10:30 +0200

    remove JPMS

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,19 +17,16 @@ repositories {
 val saduVersion = "2.3.3"
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
     implementation("net.dv8tion:JDA:5.6.1") {
         exclude(module = "opus-java")
     }
-
-    implementation("org.jspecify:jspecify:1.0.0")
     implementation("club.minnced:discord-webhooks:0.8.4")
     implementation("io.github.kaktushose:jda-commands:4.0.0-beta.8")
+
     implementation("ch.qos.logback:logback-core:1.5.13")
     implementation("ch.qos.logback:logback-classic:1.5.13")
     implementation("org.slf4j:slf4j-api:2.0.9")
+
     implementation("org.postgresql:postgresql:42.7.4")
     implementation("com.zaxxer:HikariCP:6.1.0")
     implementation("de.chojo.sadu", "sadu-datasource", saduVersion)
@@ -38,6 +35,7 @@ dependencies {
     implementation("de.chojo.sadu", "sadu-postgresql", saduVersion)
     implementation("de.chojo.sadu", "sadu-updater", saduVersion)
 
+    implementation("org.jspecify:jspecify:1.0.0")
 }
 
 tasks.test {
