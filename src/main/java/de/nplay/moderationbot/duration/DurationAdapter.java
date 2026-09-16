62846ec3a58936e4b4cbc724404516ca9c83224e
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2026-09-16 14:11:22 +0200

    remove unwanted change

--- src/main/java/de/nplay/moderationbot/duration/DurationAdapter.java
+++ src/main/java/de/nplay/moderationbot/duration/DurationAdapter.java
@@ -65,7 +65,7 @@ public class DurationAdapter implements TypeAdapter<String, Duration> {
         try {
             return Optional.of(Duration.parse(parseString));
         } catch (DateTimeParseException e) {
-            log.debug("User provided invalid duration: {}", raw);
+            log.warn("User provided invalid duration: {}", raw);
             return Optional.empty();
         }
     }
